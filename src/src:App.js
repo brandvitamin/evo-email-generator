15903250e3afd101fd63495d015c10560d1e162a
brandvitamin <victoria@brandvitamin.com.au>
2025-04-08 16:38:15 +1000

    Add files via upload

---
+++ src/src:App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EmailGeneratorUpload from './components/EmailGeneratorUpload';
+
+function App() {
+  return (
+    <div className="App">
+      <EmailGeneratorUpload />
+    </div>
+  );
+}
+
+export default App;
